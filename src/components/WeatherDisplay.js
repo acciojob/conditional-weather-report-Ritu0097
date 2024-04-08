b766bb283e7c6cec1d38db9dbf659fd5e6e97141
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-08 23:38:42 +0530

    weather

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const WeatherDisplay = ({weather}) => {
-    const temperatureColor = weather.temperature > 20 ? "red" : "blue";
-    return (
-        <div>
-            <div>
-                <p style={{ color: temperatureColor }}>Temperature: {weather.temperature}</p>
-                <p>Conditions: {weather.conditions}</p>
-            </div>
-        </div>
-    );
+const WeatherDisplay = ({ weather }) => {
+  const temperatureColor = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <span style={{ color: temperatureColor }}>Temperature: {weather.temperature}</span>
+      <br />
+      <span>Conditions: {weather.conditions}</span>
+    </div>
+  );
 };
-export default WeatherDisplay
+
+export default WeatherDisplay;
